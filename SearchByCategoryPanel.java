c45ad184f29225aead5e886843b9d2a3f8f0d935
trocks6 <trocks6@hotmail.com>
2016-10-12 20:00:03 -0500

    Updated File List

--- SearchByCategoryPanel.java
+++ SearchByCategoryPanel.java
@@ -1,4 +1,14 @@
+import javax.swing.*;
 
 public class SearchByCategoryPanel extends JPanel{
+	
+	int    booksAvailable;
+	
+	double bookCost;
+	
+	String category;
+	String bookName;
+	String bookSeries;
+	
 
 }
